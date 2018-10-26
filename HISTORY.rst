55c2220513a28bb85c4c09a87360dfaf2d6a1de7
Sasho Savkov <me@sasho.io>
2018-10-27 00:10:49 +0100

    pypi stuff

--- HISTORY.rst
+++ HISTORY.rst
@@ -3,6 +3,21 @@
 Release History
 ---------------
 
+0.1.4
+++++++++++++++++++
+
+- fixed markdown in long description
+
+0.1.3
+++++++++++++++++++
+
+- fixed markdown in long description
+
+0.1.2
+++++++++++++++++++
+
+- updated long description
+
 0.1.1
 ++++++++++++++++++
 
--- setup.py
+++ setup.py
@@ -9,11 +9,16 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 with open('requirements.txt') as fh:
     install_requires = fh.read().splitlines()
+with open('README.md') as fh:
+    readme = fh.read()
+
 
 setup(
     name='freebie',
-    version='0.1.1',
+    version='0.1.4',
     description='A free database of news resources for information extraction',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     author='Aleksandar Savkov',
     packages=find_packages(exclude=('test',)),
     zip_safe=True,
@@ -21,7 +26,6 @@ setup(
     license='GPL3',
     install_requires=install_requires,
     classifiers=[
-        'Intended Audience :: Information Extraction Researchers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
