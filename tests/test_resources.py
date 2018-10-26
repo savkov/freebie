b29023e5cfec7ebb22a4c4a22a6d957d59a84122
Sasho Savkov <sasho.savkov@babylonhealth.com>
2018-10-26 23:28:11 +0100

    removed ignore field

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -2,7 +2,7 @@ from freebie import websources, twitter_handles, rss_feeds
 
 
 def test_resources():
-    assert len(websources[0].keys()) == 8
+    assert len(websources[0].keys()) == 7
     assert len(twitter_handles[0].keys()) == 8
     assert len(rss_feeds[0].keys()) == 10
     assert 'name' in websources[0].keys()
