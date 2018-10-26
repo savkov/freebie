ba021970e88c31f8fe809c25988d2312919c2a6e
Sasho Savkov <sasho.savkov@babylonhealth.com>
2018-10-26 23:12:25 +0100

    Updated the meta

--- HISTORY.rst
+++ HISTORY.rst
@@ -3,7 +3,14 @@
 Release History
 ---------------
 
-0.1.0 (unreleased)
+0.1.1
+++++++++++++++++++
+
+- Added more websources, twitter handles & RSS feeds
+- Added tests
+- Restructured the package
+
+0.1.0
 ++++++++++++++++++
 
 - Initial release
--- setup.py
+++ setup.py
@@ -10,12 +10,9 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 with open('requirements.txt') as fh:
     install_requires = fh.read().splitlines()
 
-with open('VERSION') as fh:
-    version = fh.read().strip()
-
 setup(
     name='freebie',
-    version=version,
+    version='0.1.1',
     description='A free database of news resources for information extraction',
     author='Aleksandar Savkov',
     packages=find_packages(exclude=('test',)),
